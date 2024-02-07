909dc574133a33c19ef27634c15a1c531a404dbb
Dapperson <ronimerdiansyah@gmail.com>
2024-02-07 10:25:28 +0700

    ubah struktur

--- data_preparation.py
+++ data_preparation.py
@@ -1,19 +1,7 @@
 import pandas as pd
-<<<<<<< HEAD:data_preparation.py
-# import os
 
-# data_folder = os.path.abspath(os.path.join(os.getcwd(), os.pardir, 'data'))
-
-# data_user_combined = pd.read_excel(os.path.join(data_folder, 'data_user_combined.xlsx'))
-# combined_place_data = pd.read_excel(os.path.join(data_folder, 'combined_place_data.xlsx'))
-
-data_user_combined = pd.read_csv('.\data\data_user_combined_v2.csv')
-combined_place_data = pd.read_csv('.\data\combined_place_data_v2.csv')
-=======
-
-data_user_combined = pd.read_csv('..\data\data_user_combined_v2.csv')
-combined_place_data = pd.read_csv('..\data\combined_place_data_v2.csv')
->>>>>>> 778867cbffc22e67908a2ff040d534aa799e2da6:notebook/data_preparation.py
+data_user_combined = pd.read_csv('./data/data_user_combined_v2.csv')
+combined_place_data = pd.read_csv('./data/combined_place_data_v2.csv')
 
 new_data_user = data_user_combined.copy()
 new_data_place = combined_place_data.copy()
